2c6634283f5dea0020bf342907f998e93f3f3c3f
James Halliday <mail@substack.net>
2011-06-11 16:38:44 -0700

    test for microwave

---
+++ test/microwave.js
@@ -0,0 +1,26 @@
+var assert = require('assert');
+var burrito = require('burrito');
+
+exports.microwave = function () {
+    var times = 0;
+    var context = {
+        f : function (x) { return x + 1 },
+        g : function (x) { return x + 2 },
+        h : function (x) { return x + 3 },
+        z : function (x) {
+            times ++;
+            return x * 10;
+        },
+    };
+    
+    var res = burrito.microwave('f(g(h(5)))', context, function (node) {
+        if (node.name === 'call') {
+            node.wrap(function (s) {
+                return 'z(' + s + ')';
+            });
+        }
+    });
+    
+    assert.equal(res, (((((5 + 3) * 10) + 2) * 10) + 1) * 10);
+    assert.equal(times, 3);
+};
